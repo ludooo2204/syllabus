29b26b52ea608cc635b8fb4152628685a7c740ed
ludooo2204 <vachon.ludovic@gmail.com>
2021-12-24 15:51:36 +0100

    first commit

---
+++ App.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import Test from './components/Test'
+
+
+
+const App = () => {
+
+  return (
+  <Test />
+  );
+};
+
+
+export default App;
---
+++ components/Test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {Text,StyleSheet, View, Button, Pressable} from 'react-native';
+import Tts from 'react-native-tts';
+
+const Test = () => {
+  const syllabes = [
+    'ba',
+    'pa',
+    'ta',
+    'ka',
+    'sa',
+    'ma',
+    'na',
+    'za',
+    'la',
+    'fa',
+    'ra',
+  ];
+
+
+  return (
+    <View>
+      {syllabes.map(e => (
+        <Pressable style={styles.Button} onPress={() => Tts.speak(e)}>
+          <Text style={styles.text}>{e}</Text>
+        </Pressable>
+      ))}
+    </View>
+  );
+};
+
+export default Test;
+
+
+const styles = StyleSheet.create({
+    Button: {
+      margin: 30,
+    },
+    text:{
+        fontSize:50,
+        fontWeight:"900"
+    }
+})
